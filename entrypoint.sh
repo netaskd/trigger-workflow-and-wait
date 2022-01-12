7a595e9dce92787007edbafde92a0cc4a35c9d35
netaskd <netaskd@gmail.com>
2022-01-13 02:22:48 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -172,7 +172,7 @@ wait_for_workflow_to_finish() {
 
   if [[ "${conclusion}" == "success" && "${status}" == "completed" ]]
   then
-    echo "== Status is [completed]. All done!"
+    echo "== Success. All done!"
   else
     # Alternative "failure"
     echo "Conclusion is not success, it's [${conclusion}]."
